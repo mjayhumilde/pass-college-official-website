8c980fcc0f132d481b90d60a8bf90e3978f21df0
Mark John Humilde <markjohnhumilde14@gmail.com>
2025-05-09 12:13:47 +0800

    fix the file structure of news dummy data and added new photos for better presentation

---
+++ src/data/news-events/news.js
@@ -0,0 +1,111 @@
+import news2_1 from "../../assets/images/news-events/news/news-1-1.jpg";
+import news2_2 from "../../assets/images/news-events/news/news1-2.jpg";
+import news2_3 from "../../assets/images/news-events/news/news1-3.jpg";
+import news2_4 from "../../assets/images/news-events/news/news1-4.jpg";
+import news2_5 from "../../assets/images/news-events/news/news1-5.jpg";
+import news2_6 from "../../assets/images/news-events/news/news1-6.jpg";
+import news2_7 from "../../assets/images/news-events/news/news1-7.jpg";
+import news2_8 from "../../assets/images/news-events/news/news1-8.jpg";
+import news2_9 from "../../assets/images/news-events/news/news1-9.jpg";
+import news2_10 from "../../assets/images/news-events/news/news1-10.jpg";
+import news2_11 from "../../assets/images/news-events/news/news1-11.jpg";
+import news2_12 from "../../assets/images/news-events/news/news1-12.jpg";
+
+import news3_1 from "../../assets/images/news-events/news/news2-1.jpg";
+
+import news4_1 from "../../assets/images/news-events/news/news3-1.jpg";
+import news4_2 from "../../assets/images/news-events/news/news3-2.jpg";
+import news4_3 from "../../assets/images/news-events/news/news3-3.jpg";
+import news4_4 from "../../assets/images/news-events/news/news3-4.jpg";
+import news4_5 from "../../assets/images/news-events/news/news3-5.jpg";
+import news4_6 from "../../assets/images/news-events/news/news3-6.jpg";
+import news4_7 from "../../assets/images/news-events/news/news3-7.jpg";
+import news4_8 from "../../assets/images/news-events/news/news3-8.jpg";
+import news4_9 from "../../assets/images/news-events/news/news3-9.jpg";
+import news4_10 from "../../assets/images/news-events/news/news3-10.jpg";
+import news4_11 from "../../assets/images/news-events/news/news3-11.jpg";
+import news4_12 from "../../assets/images/news-events/news/news3-12.jpg";
+import news4_13 from "../../assets/images/news-events/news/news3-13.jpg";
+import news4_14 from "../../assets/images/news-events/news/news3-14.jpg";
+
+import news5_1 from "../../assets/images/news-events/news/news4-1.jpg";
+
+export let news = [
+  {
+    id: 0,
+    img: [
+      `${news2_1}`,
+      `${news2_2}`,
+      `${news2_3}`,
+      `${news2_4}`,
+      `${news2_5}`,
+      `${news2_6}`,
+      `${news2_7}`,
+      `${news2_8}`,
+      `${news2_9}`,
+      `${news2_10}`,
+      `${news2_11}`,
+      `${news2_12}`,
+    ],
+    message:
+      "title lies must be kind bruhhh you feuhf ehesfne esf esf esfefesfgesfesfevesv asf efwefea h",
+    date: "March 9, 2025",
+  },
+  {
+    id: 1,
+    img: [`${news3_1}`],
+    message:
+      "title lies must be kind bruhhh you feuhf ehesfne esf esf esfefesfgesfesfevesv asf efwefea h",
+    date: "March 9, 2025",
+  },
+  {
+    id: 2,
+    img: [
+      `${news4_1}`,
+      `${news4_2}`,
+      `${news4_3}`,
+      `${news4_4}`,
+      `${news4_5}`,
+      `${news4_6}`,
+      `${news4_7}`,
+      `${news4_8}`,
+      `${news4_9}`,
+      `${news4_10}`,
+      `${news4_11}`,
+      `${news4_12}`,
+      `${news4_13}`,
+      `${news4_14}`,
+    ],
+    message:
+      "title lies must be kind bruhhh you feuhf ehesfne esf esf esfefesfgesfesfevesv asf efwefea h",
+    date: "March 9, 2025",
+  },
+  {
+    id: 3,
+    img: [`${news5_1}`],
+    message:
+      "title lies must be kind bruhhh you feuhf ehesfne esf esf esfefesfgesfesfevesv asf efwefea h",
+    date: "March 9, 2025",
+  },
+  ////////////////////////////
+  //   {
+  //     id: 1,
+  //     img: [
+  //       "https://static.vecteezy.com/system/resources/thumbnails/036/324/708/small/ai-generated-picture-of-a-tiger-walking-in-the-forest-photo.jpg",
+  //       "https://t4.ftcdn.net/jpg/00/76/80/97/360_F_76809767_Gb6A91Jm9DvdFe6UuUHQkzhcUyYjZCJf.jpg",
+  //       "https://t3.ftcdn.net/jpg/02/73/22/74/360_F_273227473_N0WRQuX3uZCJJxlHKYZF44uaJAkh2xLG.jpg",
+  //       "https://genesisimperialarts.com/wp-content/uploads/2022/11/contemporary-art-image.webp",
+  //       "https://www.shutterstock.com/image-illustration/english-bulldog-portrait-intellectual-art-600nw-1926395366.jpg",
+  //       "https://www.shutterstock.com/image-illustration/english-bulldog-portrait-intellectual-art-600nw-1926395366.jpg",
+  //       "https://st2.depositphotos.com/3584689/10822/i/450/depositphotos_108229416-stock-illustration-couple-walking-on-harbor-pier.jpg",
+  //       "https://t4.ftcdn.net/jpg/00/76/80/97/360_F_76809767_Gb6A91Jm9DvdFe6UuUHQkzhcUyYjZCJf.jpg",
+  //       "https://t3.ftcdn.net/jpg/02/73/22/74/360_F_273227473_N0WRQuX3uZCJJxlHKYZF44uaJAkh2xLG.jpg",
+  //       "https://genesisimperialarts.com/wp-content/uploads/2022/11/contemporary-art-image.webp",
+  //       "https://www.shutterstock.com/image-illustration/english-bulldog-portrait-intellectual-art-600nw-1926395366.jpg",
+  //       "https://www.shutterstock.com/image-illustration/english-bulldog-portrait-intellectual-art-600nw-1926395366.jpg",
+  //     ],
+  //     message:
+  //       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore repellendus doloribus aliquid illum vel sequi velit dolorem saepe molestiae, eos voluptate officia! Officia eum ad eveniet quaerat qui a magnam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti, consectetur asperiores ut recusandae est ad velit vero non, repellat quis eos accusantium aut atque alias labore aliquid voluptatum excepturi! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus illum provident saepe placeat alias. Aspernatur illum a maiores, excepturi necessitatibus temporibus nulla. Odit eos assumenda a asperiores maxime, consequuntur nam?",
+  //     date: "March 19, 2025",
+  //   },
+];
