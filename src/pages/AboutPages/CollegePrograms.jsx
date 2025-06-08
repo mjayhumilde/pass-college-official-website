4f8abc72550b947b96788c4771939bfc2ce49d9c
Mark John Humilde <markjohnhumilde14@gmail.com>
2025-06-08 14:28:43 +0800

    added animatin to the whole CollegePrograms Page

--- src/pages/AboutPages/CollegePrograms.jsx
+++ src/pages/AboutPages/CollegePrograms.jsx
@@ -14,6 +14,11 @@ import CourseRecommendationQuiz from "../../components/ourseRecommendationQuiz";
 
 import useAuthStore from "../../store/useAuthStore";
 import { useEffect } from "react";
+import SectionAnimator from "../../components/SectionAnimator";
+import PopUpAnimation from "../../components/PopUpAnimation";
+import LeftAnimation from "../../components/LeftAnimation";
+import OpacityAnimation from "../../components/OpacityAnimation";
+import RightAnimation from "../../components/RightAnimation";
 
 const CollegePrograms = () => {
   // img adrs  https://scontent.fcrk1-5.fna.fbcdn.net/v/t39.30808-6/484880416_1210267821105051_5144354052467757109_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=5j_klYeW5cYQ7kNvgEnhVd1&_nc_oc=AdkoPZnxj9XuHvmIhr0fXdCtW4b7Lr1MA0bjvqPt6_V_v39N8R68kSo3AUbZU-H8C8U&_nc_zt=23&_nc_ht=scontent.fcrk1-5.fna&_nc_gid=GlSRuY223SOsqWSVovofsg&oh=00_AYEgS2xiOv59NaAD7xt7nRQcT0fYtRmvYQAgiSASWm4G_A&oe=67E28E91
@@ -35,51 +40,60 @@ const CollegePrograms = () => {
       )}
       <HeroBgSection img={bgCollegeProgram} label={"College programs"} />
 
-      <SectionUnbalance
-        label={"Your Future Starts with the Right Choice"}
-        description={
-          "Discover the perfect program that aligns with your passion and career goals. At PASS College, we offer a diverse range of courses designed to equip you with the skills and knowledge for success. Start your journey today and turn your aspirations into reality!"
-        }
-      />
+      <SectionAnimator>
+        <SectionUnbalance
+          label={"Your Future Starts with the Right Choice"}
+          description={
+            "Discover the perfect program that aligns with your passion and career goals. At PASS College, we offer a diverse range of courses designed to equip you with the skills and knowledge for success. Start your journey today and turn your aspirations into reality!"
+          }
+        />
+      </SectionAnimator>
 
       <section className="2xl:container 2xl:mx-auto">
         <div className="pt-16 pb-5 text-center">
-          <h2 className="text-2xl font-bold tracking-wider md:text-3xl text-red-primary">
-            DISCOVER, LEARN, AND SUCCEED
-          </h2>
+          <PopUpAnimation>
+            <h2 className="text-2xl font-bold tracking-wider md:text-3xl text-red-primary">
+              DISCOVER, LEARN, AND SUCCEED
+            </h2>
+          </PopUpAnimation>
           <div className="w-20 mx-auto mt-2 border-b-2 md:w-44 border-red-950"></div>
         </div>
 
         <section className="flex flex-col-reverse lg:grid lg:grid-cols-2">
           <div className="relative lg:h-[500px] bg-cover  bg-no-repeat">
             <div className="top-0 bottom-0 left-0 right-0 flex items-center justify-center h-full p-5 text-red-50 bg-red-primary sm:p-10 sm:px-16 sm:py-6 lg:p-20">
-              <div className="container h-auto mx-auto space-y-4 lg:space-y-7">
-                <h2 className="text-3xl font-bold">
-                  {" "}
-                  Bachelor of Science in Accountancy
-                </h2>
-                <p className="w-5/6 sm:w-4/6">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus debitis ratione odio repellat sequ
-                </p>
-              </div>
+              <LeftAnimation>
+                <div className="container h-auto mx-auto space-y-4 lg:space-y-7">
+                  <h2 className="text-3xl font-bold">
+                    Bachelor of Science in Accountancy
+                  </h2>
+                  <p className="w-5/6 sm:w-4/6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusamus debitis ratione odio repellat sequ
+                  </p>
+                </div>
+              </LeftAnimation>
             </div>
           </div>
-          <div
-            className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
-            style={{
-              backgroundImage: `url(${alter1})`,
-            }}
-          ></div>
+          <OpacityAnimation>
+            <div
+              className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
+              style={{
+                backgroundImage: `url(${alter1})`,
+              }}
+            ></div>
+          </OpacityAnimation>
         </section>
 
         <section className="lg:grid lg:grid-cols-2">
-          <div
-            className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
-            style={{
-              backgroundImage: `url(${alter2})`,
-            }}
-          ></div>
+          <OpacityAnimation>
+            <div
+              className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
+              style={{
+                backgroundImage: `url(${alter2})`,
+              }}
+            ></div>
+          </OpacityAnimation>
           <div
             className="relative lg:h-[500px] bg-cover  bg-no-repeat"
             style={{
@@ -87,16 +101,18 @@ const CollegePrograms = () => {
             }}
           >
             <div className="top-0 bottom-0 left-0 right-0 flex items-center justify-center h-full p-5 text-red-50 bg-gray-secondary-opacity sm:p-10 sm:px-16 sm:py-6 lg:p-10">
-              <div className="container mx-auto space-y-4 lg:space-y-7">
-                <h2 className="text-3xl font-bold">
-                  Bachelor of Science in Business Administration
-                </h2>
-                <p className="w-5/6 sm:w-4/6">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus debitis ratione odio repellat sequi molestias
-                  quaerat amet blanditiis magni vero voluptates quo neque pro
-                </p>
-              </div>
+              <RightAnimation>
+                <div className="container mx-auto space-y-4 lg:space-y-7">
+                  <h2 className="text-3xl font-bold">
+                    Bachelor of Science in Business Administration
+                  </h2>
+                  <p className="w-5/6 sm:w-4/6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusamus debitis ratione odio repellat sequi molestias
+                    quaerat amet blanditiis magni vero voluptates quo neque pro
+                  </p>
+                </div>
+              </RightAnimation>
             </div>
           </div>
         </section>
@@ -104,33 +120,38 @@ const CollegePrograms = () => {
         <section className="flex flex-col-reverse lg:grid lg:grid-cols-2">
           <div className="relative lg:h-[500px] bg-cover  bg-no-repeat">
             <div className="top-0 bottom-0 left-0 right-0 flex items-center justify-center h-full p-5 text-red-50 bg-red-primary sm:p-10 sm:px-16 sm:py-6 lg:p-20">
-              <div className="container h-auto mx-auto space-y-4 lg:space-y-7">
-                <h2 className="text-3xl font-bold">
-                  {" "}
-                  Bachelor of Science in Computer Science
-                </h2>
-                <p className="w-5/6 sm:w-4/6">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus debitis ratione odio repellat sequ
-                </p>
-              </div>
+              <LeftAnimation>
+                <div className="container h-auto mx-auto space-y-4 lg:space-y-7">
+                  <h2 className="text-3xl font-bold">
+                    Bachelor of Science in Computer Science
+                  </h2>
+                  <p className="w-5/6 sm:w-4/6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusamus debitis ratione odio repellat sequ
+                  </p>
+                </div>
+              </LeftAnimation>
             </div>
           </div>
-          <div
-            className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
-            style={{
-              backgroundImage: `url(${alter3})`,
-            }}
-          ></div>
+          <OpacityAnimation>
+            <div
+              className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
+              style={{
+                backgroundImage: `url(${alter3})`,
+              }}
+            ></div>
+          </OpacityAnimation>
         </section>
 
         <section className="lg:grid lg:grid-cols-2">
-          <div
-            className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
-            style={{
-              backgroundImage: `url(${alter4})`,
-            }}
-          ></div>
+          <OpacityAnimation>
+            <div
+              className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
+              style={{
+                backgroundImage: `url(${alter4})`,
+              }}
+            ></div>
+          </OpacityAnimation>
           <div
             className="relative lg:h-[500px] bg-cover  bg-no-repeat"
             style={{
@@ -138,19 +159,21 @@ const CollegePrograms = () => {
             }}
           >
             <div className="top-0 bottom-0 left-0 right-0 flex items-center justify-center h-full p-5 text-red-50 bg-gray-secondary-opacity sm:p-10 sm:px-16 sm:py-6 lg:p-10">
-              <div className="container mx-auto space-y-4 lg:space-y-7">
-                <h2 className="text-3xl font-bold">
-                  {" "}
-                  Bachelor of Science in Criminology
-                </h2>
-                <p className="w-5/6 sm:w-4/6">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus debitis ratione odio repellat sequi molestias
-                  quaerat amet blanditiis magni vero voluptates quo neque pro
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Placeat architecto reprehend
-                </p>
-              </div>
+              <RightAnimation>
+                <div className="container mx-auto space-y-4 lg:space-y-7">
+                  <h2 className="text-3xl font-bold">
+                    {" "}
+                    Bachelor of Science in Criminology
+                  </h2>
+                  <p className="w-5/6 sm:w-4/6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusamus debitis ratione odio repellat sequi molestias
+                    quaerat amet blanditiis magni vero voluptates quo neque pro
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Placeat architecto reprehend
+                  </p>
+                </div>
+              </RightAnimation>
             </div>
           </div>
         </section>
@@ -158,33 +181,38 @@ const CollegePrograms = () => {
         <section className="flex flex-col-reverse lg:grid lg:grid-cols-2">
           <div className="relative lg:h-[500px] bg-cover  bg-no-repeat">
             <div className="top-0 bottom-0 left-0 right-0 flex items-center justify-center h-full p-5 text-red-50 bg-red-primary sm:p-10 sm:px-16 sm:py-6 lg:p-20">
-              <div className="container h-auto mx-auto space-y-4 lg:space-y-7">
-                <h2 className="text-3xl font-bold">
-                  {" "}
-                  Bachelor of Science in Elementary Education
-                </h2>
-                <p className="w-5/6 sm:w-4/6">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus debitis ratione odio repellat sequ
-                </p>
-              </div>
+              <LeftAnimation>
+                <div className="container h-auto mx-auto space-y-4 lg:space-y-7">
+                  <h2 className="text-3xl font-bold">
+                    Bachelor of Science in Elementary Education
+                  </h2>
+                  <p className="w-5/6 sm:w-4/6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusamus debitis ratione odio repellat sequ
+                  </p>
+                </div>
+              </LeftAnimation>
             </div>
           </div>
-          <div
-            className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
-            style={{
-              backgroundImage: `url(${alter5})`,
-            }}
-          ></div>
+          <OpacityAnimation>
+            <div
+              className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
+              style={{
+                backgroundImage: `url(${alter5})`,
+              }}
+            ></div>
+          </OpacityAnimation>
         </section>
 
         <section className="lg:grid lg:grid-cols-2">
-          <div
-            className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
-            style={{
-              backgroundImage: `url(${alter6})`,
-            }}
-          ></div>
+          <OpacityAnimation>
+            <div
+              className=" h-[400px] lg:h-[500px] bg-cover  bg-no-repeat"
+              style={{
+                backgroundImage: `url(${alter6})`,
+              }}
+            ></div>
+          </OpacityAnimation>
           <div
             className="relative lg:h-[500px] bg-cover  bg-no-repeat"
             style={{
@@ -192,32 +220,38 @@ const CollegePrograms = () => {
             }}
           >
             <div className="top-0 bottom-0 left-0 right-0 flex items-center justify-center h-full p-5 text-red-50 bg-gray-secondary-opacity sm:p-10 sm:px-16 sm:py-6 lg:p-10">
-              <div className="container mx-auto space-y-4 lg:space-y-7">
-                <h2 className="text-3xl font-bold">
-                  {" "}
-                  Bachelor of Science in Tourism Management
-                </h2>
-                <p className="w-5/6 sm:w-4/6">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus debitis ratione odio repellat sequi molestias
-                  quaerat amet blanditiis magni vero voluptates quo neque pro
-                </p>
-              </div>
+              <RightAnimation>
+                <div className="container mx-auto space-y-4 lg:space-y-7">
+                  <h2 className="text-3xl font-bold">
+                    {" "}
+                    Bachelor of Science in Tourism Management
+                  </h2>
+                  <p className="w-5/6 sm:w-4/6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusamus debitis ratione odio repellat sequi molestias
+                    quaerat amet blanditiis magni vero voluptates quo neque pro
+                  </p>
+                </div>
+              </RightAnimation>
             </div>
           </div>
         </section>
       </section>
 
       <section>
-        <SectionUnbalance
-          label={"Facilities & Learning Environment"}
-          description={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus debitis ratione odio repellat sequi molestias quaerat amet blanditiis magni vero voluptates quo neque proLorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus debitis ratione odio repellat sequi molestias quaerat amet blanditiis magni vero voluptates quo neque pro"
-          }
-        />
+        <SectionAnimator>
+          <SectionUnbalance
+            label={"Facilities & Learning Environment"}
+            description={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus debitis ratione odio repellat sequi molestias quaerat amet blanditiis magni vero voluptates quo neque proLorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus debitis ratione odio repellat sequi molestias quaerat amet blanditiis magni vero voluptates quo neque pro"
+            }
+          />
+        </SectionAnimator>
 
         <div className="mb-3 sm:mb-5">
-          <CardSlider cards={cards} />
+          <LeftAnimation>
+            <CardSlider cards={cards} />
+          </LeftAnimation>
         </div>
       </section>
     </div>
