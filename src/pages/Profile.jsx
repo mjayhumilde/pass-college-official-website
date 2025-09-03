7f65f5bfe4c111edc7c7c987bd22b3ed62790e59
Mark John Humilde <markjohnhumilde14@gmail.com>
2025-09-03 15:45:22 +0800

    display the user data of log in user in profile and integrate the api endpoint for updating user infos

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,52 +1,48 @@
+import { useState, useEffect } from "react";
+import { Edit, Camera, LogOutIcon } from "lucide-react";
 import useAuthStore from "../store/useAuthStore";
 import { useNavigate } from "react-router-dom";
-import { useState, useRef, useEffect } from "react";
-
-import { Edit, Camera, LogOutIcon } from "lucide-react";
 
 const Profile = () => {
-  const { logout } = useAuthStore();
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  const userRole = useAuthStore((state) => state.userRole);
-
+  const { logout, isAuthenticated, user, updateMe } = useAuthStore();
   const navigate = useNavigate();
-
-  // use data || MOCK DATA
-  const [userData, setUserData] = useState({
-    firstName: "Mark John",
-    lastName: "Humilde",
-    email: "humildemarkjohn00@gmail.com",
-    course: "Computer Science",
-    role: "Student",
-    profileImage:
-      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-  });
 
   const [isEditing, setIsEditing] = useState(false);
 
-  // File input reference
-  const fileInputRef = useRef(null);
+  // To ensure that we always have the updated user in the UI
+  const [userData, setUserData] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    course: user.course,
+    photo: user.photo,
+  });
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      // upload this file to a server
-      // for now just create a local URL to display the image
-      const imageUrl = URL.createObjectURL(file);
-      setUserData({
-        ...userData,
-        profileImage: imageUrl,
-      });
-    }
-  };
+  // Reactively update the profile picture
+  const profileImageUrl = user?.photo
+    ? `http://127.0.0.1:5000${user.photo}`
+    : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"; // Fallback image
 
-  const handleProfileImageClick = () => {
-    fileInputRef.current.click();
-  };
+  useEffect(() => {
+    // Update userData state whenever the user in the store is updated
+    setUserData({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      course: user.course,
+      photo: user.photo,
+    });
+  }, [user]); // Dependency on 'user' will trigger when user changes in the store
 
-  // Handle save changes
-  const handleSaveChanges = () => {
-    console.log(userData);
+  // Handle save changes and update user
+  const handleSaveChanges = async () => {
+    await updateMe(
+      userData.firstName,
+      userData.lastName,
+      userData.email,
+      userData.course,
+      userData.photo
+    );
 
     setIsEditing(false);
   };
@@ -64,28 +60,39 @@ const Profile = () => {
           <div className="flex flex-col items-center gap-6 md:flex-row">
             {/* Profile Image with Upload Functionality */}
             <div className="relative mb-4 md:mb-0">
-              <div
-                className="w-32 h-32 mx-auto overflow-hidden border-4 border-white rounded-full shadow-md cursor-pointer sm:w-36 sm:h-36"
-                onClick={handleProfileImageClick}
-              >
-                <img
-                  src={userData.profileImage}
-                  alt="Profile"
-                  className="object-cover w-full h-full"
-                />
-                <div className="absolute inset-0 bg-[rgba(128,0,0,0.6)] hover:opacity-100 md:bg-[rgba(128,0,0,0)] md:hover:bg-[rgba(128,0,0,0.7)] opacity-100 md:opacity-0 md:hover:opacity-100 rounded-full flex items-center justify-center transition-colors">
-                  <Camera size={40} className="text-white" />
+              {!isEditing ? (
+                <div className="w-32 h-32 mx-auto overflow-hidden border-4 border-white rounded-full shadow-md cursor-pointer sm:w-36 sm:h-36">
+                  <img
+                    src={profileImageUrl} // Using the full URL for the profile image
+                    alt="Profile"
+                    className="object-cover w-full h-full"
+                  />
                 </div>
-              </div>
-              <input
-                type="file"
-                ref={fileInputRef}
-                className="hidden"
-                accept="image/*"
-                onChange={handleFileChange}
-              />
+              ) : (
+                <div>
+                  <div className="w-32 h-32 mx-auto overflow-hidden border-4 border-white rounded-full shadow-md cursor-pointer sm:w-36 sm:h-36">
+                    <img
+                      src={profileImageUrl} // Using the full URL for the profile image
+                      alt="Profile"
+                      className="object-cover w-full h-full"
+                    />
+                  </div>
+                  <div className="text-center mt-3 text-white">
+                    <input
+                      type="file"
+                      onChange={(e) => {
+                        const file = e.target.files[0];
+                        setUserData({
+                          ...userData,
+                          photo: file,
+                        });
+                      }}
+                      className="bg-red-900 p-2 rounded-full w-30"
+                    />
+                  </div>
+                </div>
+              )}
             </div>
-
             {/* User Information */}
             <div className="flex-1 w-full">
               <div className="flex flex-col justify-between gap-2 mb-6 sm:flex-row sm:items-center">
@@ -166,26 +173,13 @@ const Profile = () => {
                     <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
                       <div>
                         <label className="block mb-1 text-sm font-medium text-gray-700">
-                          Course
+                          Department
                         </label>
                         <input
                           type="text"
                           value={userData.course}
                           onChange={(e) =>
                             setUserData({ ...userData, course: e.target.value })
-                          }
-                          className="block w-full py-2 pl-10 pr-10 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm text-red-primary focus:outline-none focus:ring-red-800 focus:border-red-800 sm:text-sm"
-                        />
-                      </div>
-                      <div className="hidden">
-                        <label className="block mb-1 text-sm font-medium text-gray-700">
-                          Role
-                        </label>
-                        <input
-                          type="text"
-                          value={userData.role}
-                          onChange={(e) =>
-                            setUserData({ ...userData, year: e.target.value })
                           }
                           className="block w-full py-2 pl-10 pr-10 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm text-red-primary focus:outline-none focus:ring-red-800 focus:border-red-800 sm:text-sm"
                         />
@@ -222,7 +216,7 @@ const Profile = () => {
                     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                       <div>
                         <p className="text-sm font-medium text-gray-500">
-                          Course
+                          Department
                         </p>
                         <p className="font-bold text-gray-900">
                           {userData.course}
@@ -233,9 +227,9 @@ const Profile = () => {
                           Role
                         </p>
                         <p className="font-bold text-gray-900">
-                          {isAuthenticated && userRole === "user"
-                            ? userData.role
-                            : isAuthenticated && userRole === "teacher"
+                          {isAuthenticated && user.role === "student"
+                            ? user.role
+                            : isAuthenticated && user.role === "teacher"
                             ? "Teacher"
                             : "Admin"}
                         </p>
